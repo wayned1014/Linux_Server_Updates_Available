afc219e40fc54e5b79d5a314223157a16d1d451d
Wayne DeBiasio <wayne1014@yahoo.com>
2022-07-27 01:39:54 -0400

    Moved configuration to an external file.

--- apt-count.py
+++ apt-count.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python3
 #coding=utf-8
 
+import configparser
 import apt
 import os
 import sys
 import signal
 import paho.mqtt.client as mqtt
 import time
+import configparser
 
-# from datetime import timedelta
-# import datetime as dt
-# import apt_pkg
-# import subprocess
-# from re import findall
-
-# This information need to be filled in
+# This is the default infomation.  Then we will read the config file
+# for the current information
 #
-broker_url = "0.0.0.0"          # MQTT server IP
-broker_port = 1883              # MQTT server port
-broker_username = "username"    # MQTT Username
-broker_password = "password"    # MQTT Password
+
+parser = configparser.ConfigParser()
+
+ini_file_name = "/usr/local/etc/apt-count-config.ini"
+
+parser.read(ini_file_name)
 
 # Set deviceName to the hostname
 deviceName = os.uname()[1]
 
-client = mqtt.Client()
-client.username_pw_set(broker_username, broker_password)
-
 class ProgramKilled(Exception):
     pass
+
+def get_config(config, section, name):
+    if config.has_option(section,name):
+        return config.get(section,name)
+    else:
+        exit("Missing "+ name + ".\nPlease try again. Exiting.")
 
 def signal_handler(signum, frame):
     raise ProgramKilled
@@ -61,6 +63,14 @@ if __name__ == '__main__':
     #
     if os.geteuid() != 0:
         exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
+
+    broker_url = get_config(parser, "settings", "broker_url")
+    broker_port = int(get_config(parser, "settings", "broker_port"))
+    broker_username = get_config(parser, "settings", "broker_username")
+    broker_password = get_config(parser, "settings", "broker_password")
+
+    client = mqtt.Client()
+    client.username_pw_set(broker_username, broker_password)
 
     pkgs = get_update_packages_count()
     print('%d updates' % pkgs)
