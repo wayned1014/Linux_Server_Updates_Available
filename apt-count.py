9bbd1be810535d0bc20608f1a7970f672119b74c
Wayne DeBiasio <wayne1014@yahoo.com>
2022-08-01 22:06:23 -0400

    Fix line spacing

--- apt-count.py
+++ apt-count.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #coding=utf-8
 
-# Version: 1.0.0
+# Version: 1.0.1
 # This script will create/update an entity in HA with the number
 # of updates available for the system.
 
